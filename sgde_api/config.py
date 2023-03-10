038accde22d7d5aeadf1dc93711e26da04c693b6
archettialberto <alberto1.archetti@mail.polimi.it>
2023-03-10 11:04:45 +0100

    Added .env support for sgde-api

--- sgde_api/config.py
+++ sgde_api/config.py
@@ -26,13 +26,16 @@ class Config(BaseSettings):
     INSTANCE_PATH = os.path.join(os.getcwd(), "instance")
     DATABASE_URL: str = f"sqlite:///{os.path.join(INSTANCE_PATH, 'sgde_db.db')}"
 
+    JWT_SECRET: str
     JWT_ALG: str = "HS256"
-    JWT_SECRET: str = "SECRET"
     JWT_EXP: int = "21000"
 
     ENVIRONMENT: Environment = Environment.DEVELOPMENT
 
     GENERATOR_PATH: str = os.path.join(os.getcwd(), "instance", "generators")
+
+    class Config:
+        env_file = ".env"
 
 
 settings = Config()
