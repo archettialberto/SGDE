48e621f0708f63e69681b416b8f9bd041fc6001a
archettialberto <alberto1.archetti@mail.polimi.it>
2023-03-10 10:55:33 +0100

    Updated Dockerfiles

--- sgde_frontend/pages/4_Explore.py
+++ sgde_frontend/pages/4_Explore.py
@@ -1,15 +1,15 @@
-from datetime import datetime
-
+import pandas as pd
+import requests
 import streamlit as st
 
-items = [{"name": f"items{i}", "size": {i}, "date": datetime.utcnow()} for i in range(100)]
+FASTAPI_ENDPOINT = "http://127.0.0.1:8000/generators"
 
-FASTAPI_ENDPOINT = "http://localhost:8000/generators"
-PAGE_SIZE = 10
+response = requests.get(FASTAPI_ENDPOINT)
 
 st.session_state.page = 1
 
 st.set_page_config(page_title="SGDE - Explore", page_icon=":earth_africa:")
 st.header(":earth_africa: Explore")
 
-st.table(items)
+generators = pd.DataFrame(response.json())
+st.table(generators)
