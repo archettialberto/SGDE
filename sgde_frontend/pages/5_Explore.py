aadd4f8ef815de85c8ef673f89900666ef73c908
archettialberto <alberto1.archetti@mail.polimi.it>
2023-03-17 17:30:01 +0100

    Updated explore page

--- sgde_frontend/pages/5_Explore.py
+++ sgde_frontend/pages/5_Explore.py
@@ -6,22 +6,41 @@ FASTAPI_ENDPOINT = "http://sgde_api:8000"
 GENERATORS_ENDPOINT = f"{FASTAPI_ENDPOINT}/generators/"
 response = requests.get(GENERATORS_ENDPOINT)
 
-st.set_page_config(page_title="SGDE - Explore", page_icon=":earth_africa:")
+st.set_page_config(page_title="SGDE - Explore", page_icon=":earth_africa:", layout="wide")
 st.header(":earth_africa: Explore")
 
 generators = response.json()
-
-
-def download_generator_fn(name: str):
-    try:
-        url = f"{GENERATORS_ENDPOINT}{name}/download"
-        requests.get(url, headers={"Authorization": f"Bearer {st.session_state['access_token']}"})
-    except KeyError:
-        st.error("You must login first.")
-
-
-for g in generators:
-    g["download"] = st.button("Download", on_click=download_generator_fn, args=(g["name"],))
-
 df = pd.DataFrame(generators)
-st.table(df)
+
+st.sidebar.subheader("Filter")
+name_filter = st.sidebar.text_input("Name contains")
+
+filtered_df = df[df["name"].str.contains(name_filter)]
+
+st.sidebar.subheader("Sort")
+sort_by = st.sidebar.selectbox("Sort by", ["name"])
+
+sorted_df = filtered_df.sort_values(by=sort_by)
+
+if len(sorted_df) == 0:
+    st.write('No generators found.')
+else:
+    columns = list(df.columns)
+    excluded_columns = ["conditioned", "batch_size", "description"]
+    for c in excluded_columns:
+        columns.remove(c)
+    cols = st.columns(len(columns) + 1)
+    for col, col_name in zip(cols[:-1], columns):
+        if col_name not in excluded_columns:
+            with col:
+                st.write(col_name)
+    with cols[-1]:
+        st.write("Download")
+    sorted_df = sorted_df.reset_index(drop=True)
+    for i in range(len(sorted_df)):
+        for col, col_name in zip(cols[:-1], columns):
+            if col_name not in excluded_columns:
+                with col:
+                    st.write(sorted_df.loc[i, col_name])
+        with cols[-1]:
+            st.download_button(label=":arrow_down:", data=sorted_df.loc[i, 'name'])
